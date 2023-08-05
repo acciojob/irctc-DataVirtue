303b63f86efd2e39f9c57b720fdfb0c2217141c2
Akshay Pawar <akshaypawar97@gmail.com>
2023-08-05 11:38:09 +0530

    third commit

--- src/main/java/com/driver/services/TrainService.java
+++ src/main/java/com/driver/services/TrainService.java
@@ -6,6 +6,7 @@ import com.driver.model.Passenger;
 import com.driver.model.Station;
 import com.driver.model.Ticket;
 import com.driver.model.Train;
+import com.driver.repository.TicketRepository;
 import com.driver.repository.TrainRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,12 +14,15 @@ import org.springframework.stereotype.Service;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TrainService {
 
     @Autowired
     TrainRepository trainRepository;
+
+
 
     public Integer addTrain(AddTrainEntryDto trainEntryDto){
 
@@ -52,6 +56,7 @@ public class TrainService {
         //Inshort : a train has totalNo of seats and there are tickets from and to different locations
         //We need to find out the available seats between the given 2 stations.
 
+
        return null;
     }
 
@@ -62,8 +67,36 @@ public class TrainService {
         //throw new Exception("Train is not passing from this station");
         //  in a happy case we need to find out the number of such people.
 
+        Optional<Train> trainOptional = trainRepository.findById(trainId);
+        if(trainOptional.get()==null){
+            throw new RuntimeException("Train Not Found");
+        }
 
-        return 0;
+        Train train = trainOptional.get();
+
+        String [] stations = train.getRoute().split(",");
+
+        // check if the train goes to correct stations
+
+        boolean stationFound = false;
+        for(String routeStation: stations) {
+
+            if (routeStation.toString().equals(stations)){
+                stationFound = true;
+            }
+        }
+
+        if(!stationFound){
+            throw new Exception("Train is not passing from this station");
+        }
+        int count  =0 ;
+        for(Ticket ticket: train.getBookedTickets()){
+            if(ticket.getFromStation().equals(stations)){
+                count++;
+            }
+        }
+        return count;
+
     }
 
     public Integer calculateOldestPersonTravelling(Integer trainId){
@@ -71,8 +104,23 @@ public class TrainService {
         //Throughout the journey of the train between any 2 stations
         //We need to find out the age of the oldest person that is travelling the train
         //If there are no people travelling in that train you can return 0
+        Optional<Train> trainOptional = trainRepository.findById(trainId);
+        if(trainOptional.get()==null){
+            throw new RuntimeException("Train Not Found");
+        }
 
-        return 0;
+        Train train = trainOptional.get();
+
+        Integer maxAge = 0;
+        for(Ticket ticket: train.getBookedTickets()){
+
+            for(Passenger passenger: ticket.getPassengersList()){
+                if(passenger.getAge()> maxAge){
+                    maxAge = passenger.getAge();
+                }
+            }
+        }
+        return maxAge;
     }
 
     public List<Integer> trainsBetweenAGivenTime(Station station, LocalTime startTime, LocalTime endTime){
@@ -82,8 +130,27 @@ public class TrainService {
         //You can assume that the date change doesn't need to be done ie the travel will certainly happen with the same date (More details
         //in problem statement)
         //You can also assume the seconds and milli seconds value will be 0 in a LocalTime format.
+        List<Train> trainList = trainRepository.findAll();
+         List<Integer> trainIdList = new ArrayList<>();
+        for(Train train: trainList){
+            String [] stations  = train.getRoute().split(",");
+            Integer stationIndex = -1;
+            for(int i=0;i<stations.length;i++){
+                if(stations[i].equals(station.toString())){
+                    stationIndex = i;
+                    break;
+                }
+            }
+            if(stationIndex==-1){
+                continue;
+            }
+            LocalTime timeReached = train.getDepartureTime().plusHours(stationIndex);
+            if(timeReached.isAfter(startTime) && timeReached.isBefore(endTime)){
+                trainIdList.add(train.getTrainId());
+            }
+        }
 
-        return null;
+        return trainIdList;
     }
 
 }
